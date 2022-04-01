bf31e1a2b34554d3002c6bb620cd84f1fd99e70b
trosbalos <trosbalos@gmail.ru>
2022-04-01 12:26:10 +0300

    2

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,2 +1,16 @@
-package PACKAGE_NAME;public class BaseTest {
+import org.openqa.selenium.By;
+import org.testng.annotations.Test;
+
+import static com.codeborne.selenide.Condition.text;
+import static com.codeborne.selenide.Selenide.$;
+import static com.codeborne.selenide.Selenide.open;
+
+public class BaseTest {
+    @Test
+    public void userCanLoginByUsername() {
+        open("https://google.com");
+        $(By.xpath("//*[@title=\"Поиск\"]")).setValue("johny");
+        $(By.xpath("//*[@value=\"Поиск в Google\"]")).click();
+        $(By.xpath("//*[@id=\"result-stats\"]")).shouldHave(text("Результатов: примерно"));
+    }
 }
